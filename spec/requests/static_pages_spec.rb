2451ae97b327b7efe0a9bd4625040ec8d1ba9dda
canberra0709 <example@xxx.com>
2014-09-02 18:53:41 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,59 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPagesのテスト" do
+describe "Static pages" do
 
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  subject { page }
 
-  describe "Home pageのテスト" do
+  describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
-  describe "Help pageのテスト" do
-  
-    it "'Help'という語がhelpに含まれていること" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+  describe "Help page" do
+    before { visit help_path }
 
-    it "タイトルの末尾が'Help'になっていること" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "About pageのテスト" do
+  describe "About page" do
+    before { visit about_path }
 
-    it "'About Us'という語がAbouに含まれていること" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "タイトルの末尾が'About'になっていること" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
-  describe "Contactのテスト" do
+  describe "Contact page" do
+    before { visit contact_path }
 
-    it "タイトルの末尾が'Contact'になっていること" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
-
 end
