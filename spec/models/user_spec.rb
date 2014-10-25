f7536c1b0be42e199e6d5bb02aa716ecdbaf1ee2
canberra0709 <example@xxx.com>
2014-10-25 23:08:25 +0900

    6章までの終了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,11 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest)}
+  it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+
+  it { should respond_to(:authenticate)}
+
   it { should be_valid}
 
   describe "when name is not present" do
